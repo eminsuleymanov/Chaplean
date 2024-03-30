50b2f37700af9b941d59ddc85b215d3171afc37e
sabinaorujova <sabinaorujva@gmail.com>
2024-03-30 20:05:12 +0400

    createbottomModalSheet

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,8 @@
 
-import 'package:chaplean/presentation/pages/liked/liked_music_page.dart';
-import 'package:chaplean/presentation/pages/search/widgets/results.dart';
-
-import 'presentation/pages/profile/profile_page.dart';
-
-import 'presentation/pages/home/home_page.dart';
-import 'presentation/pages/library/create_playlist_page.dart';
-import 'presentation/pages/search/search_page.dart';
-
 import 'package:flutter/material.dart';
 
 import 'app/app_theme.dart';
+import 'presentation/pages/library/create_playlist_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -20,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: UITheme.appTheme,
-      home:LikedMusicPage()
+      home:LibraryPage(),
     );
   }
 }
